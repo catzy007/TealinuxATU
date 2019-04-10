67c03e6207e42b891c093baf7b894f413e208465
catzy007 <catzy.rais@gmail.com>
2019-04-10 19:55:26 +0700

    unknown

--- kill.sh
+++ kill.sh
@@ -10,7 +10,7 @@ read -p "Nama : " NAMA
 PID=$(pidof $NAMA)
 
 if [[ "$PID" != "0" ]]; then
-	sh -c $NAMA & sleep 10 && sudo kill -s 15 9 $PID
+	sh -c $NAMA & sleep 15 && sudo kill -s 15 9 $PID
 	echo "Terminated!"
 	exit 212
 fi
