a71f371aada38a7d11e7dab5aa264baf84e59084
catzy007 <catzy.rais@gmail.com>
2019-04-14 23:39:09 +0700

    fix replace on wget

--- atu.sh
+++ atu.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+file="/usr/share/tealinux/TealinuxATU/atucore.sh"
+link="https://raw.githubusercontent.com/catzy007/TealinuxATU/master/atucore.sh"
 if [ "$EUID" -ne 0 ]
   then echo "Please run atu as root!"
   exit
 fi
 echo "Checking new updates. . ."
-wget -qN https://raw.githubusercontent.com/catzy007/TealinuxATU/master/atucore.sh -O /usr/share/tealinux/TealinuxATU/atucore.sh
-chmod +x /usr/share/tealinux/TealinuxATU/atucore.sh
-/usr/share/tealinux/TealinuxATU/atucore.sh $1
+rm $file
+wget -qN $link -O $file
+chmod +x $file
+$file $1
