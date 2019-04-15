87e93785ee026618f99ad19f87c11222a56e46c7
catzy007 <catzy.rais@gmail.com>
2019-04-15 18:55:10 +0700

    add comment

--- atu.sh
+++ atu.sh
@@ -2,7 +2,7 @@
 file="/usr/share/tealinux/TealinuxATU/atucore.sh"
 link="https://raw.githubusercontent.com/catzy007/TealinuxATU/master/atucore.sh"
 if [ "$EUID" -ne 0 ]
-  then echo "Please run atu as root!"
+  then echo "Cannot set some parameters"; echo "Are you root?"
   exit
 fi
 echo "Checking new updates. . ."
